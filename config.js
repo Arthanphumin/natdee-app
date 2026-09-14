590250497cf4eb3bea69ecc7df113b40f422c5af
Sutthiphumin Arthan <arthanphumin@gmail.com>
2026-09-14 23:32:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = Object.freeze({
    */
   API: {
     APPOINTMENTS:
-      "https://looked-signing-hitachi-four.trycloudflare.com/webhook/a3083faa-b717-4e24-b053-ce1bc7563a50",
+      "https://mug-nil-sen-protection.trycloudflare.com/webhook/a3083faa-b717-4e24-b053-ce1bc7563a50",
     STATS:
-      "https://looked-signing-hitachi-four.trycloudflare.com/webhook/72fb0b75-69ec-4a82-9f8b-1f895eea30f2",
+      "https://mug-nil-sen-protection.trycloudflare.com/webhook/72fb0b75-69ec-4a82-9f8b-1f895eea30f2",
   },
 });
