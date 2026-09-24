7745433fe3f82e591c18aa7f27dcfc88d1424a96
Sutthiphumin Arthan <arthanphumin@gmail.com>
2026-09-24 12:10:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = Object.freeze({
    */
   API: {
     APPOINTMENTS:
-        "https://natdee.21missionboss.online/webhook/a3083faa-b717-4e24-b053-ce1bc7563a50",
+        "https://enginenatdee.21missionboss.online/webhook/a3083faa-b717-4e24-b053-ce1bc7563a50",
     STATS:
-        "https://natdee.21missionboss.online/webhook/72fb0b75-69ec-4a82-9f8b-1f895eea30f2",
+        "https://enginenatdee.21missionboss.online/webhook/72fb0b75-69ec-4a82-9f8b-1f895eea30f2",
   },
 });
